b8432499ae7649e21d25b1595f693772a356102d
NicolasPozzer <pozzernico@gmail.com>
2026-05-23 03:36:15 -0300

    Add: tp .net file

---
+++ temp_tp2_NET/temp_tp2_NET.cs
@@ -0,0 +1,22 @@
+/*Crear un proyecto con nombre Unidad3y4 más los apellidos de los alumnos que forman el grupo.
+Ejemplo: Unidad4Apellido1Apellido2Apellido3Apellido4Apellido5 */
+
+
+
+// #2: Crear una función que muestre un mensaje pasado como parámetro y utilizarla en todos los puntos del ejercicio.
+
+
+// #3: Crear una función que pida la altura en centímetros de 5 personas guardando en un arreglo los valores,
+	//	luego recorrer el arreglo y mostrar el mas alto y el mas bajo.
+
+// #4: Hacer una función que ingresen las notas de los alumnos y los guarde en un ArrayList. Luego calcular y 
+	//	mostrar el promedio, nota máxima y mínima.
+
+
+// #5: Hacer una función que actúe como un diccionario en donde el usuario ingresa una palabra y nos devuelve
+	//	la definición usando la colección Hashtable (inicializar con al menos 5 palabras).
+
+
+// #6: Hacer un función que le solicite al usuario 4 números y luego los muestre en orden descendente.
+
+
